461a55a0317d3429abb62943fa56d81fb6c70963
Asma Mehdizadeh <mehdizadeh.asma@gmail.com>
2023-09-04 02:44:27 +0330

    Add SSL & Remove Read Port From .env

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,12 +8,23 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import fs from "fs";
+import http from "http";
+import https from "https";
 
 import marzbanRouter from "./routes/MarzbanRouter";
 import sellerRouter from "./routes/SellerRouter";
 import tariffRouter from "./routes/TariffRouter";
+import accountRouter from "./routes/AccountRouter";
+
+const privateKey = fs.readFileSync("certs/key.pem");
+const certificate = fs.readFileSync("certs/fullchain.pem");
+const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 app.use(bodyParser.json());
 
@@ -29,6 +40,8 @@ app.use("/api/marzban", cors(corsOptions), marzbanRouter);
 app.use("/api", cors(corsOptions), sellerRouter);
 
 app.use("/api", cors(corsOptions), tariffRouter);
+
+app.use("/api", cors(corsOptions), accountRouter);
 
 app.use(
   (
@@ -50,12 +63,7 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "app is running!" });
 });
 
-let portNumber = "8080";
-const host = "localhost";
 let connectionString = "";
-
-if (process.env.BACKEND_PORT && process.env.BACKEND_PORT != "")
-  portNumber = process.env.BACKEND_PORT;
 
 if (
   process.env.MONGODB_CONNECTION_STRING &&
@@ -70,8 +78,10 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(connectionString)
   .then(() => {
-    app.listen(portNumber);
-    console.log(`Server is Startig at http://${host}:${portNumber}`);
+    httpServer.listen(8080);
+    httpsServer.listen(8443);
+    console.log(`Server is Startig at http://localhost:8080`);
+    console.log(`Server is Startig at https://localhost:8443`);
   })
   .catch((err: Error) => {
     console.log(`MongoDb Connection :  ${err.message}`);
